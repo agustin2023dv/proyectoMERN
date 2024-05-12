8851aa5604f55b6f1c9835620a888408c2746d80
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2024-05-13 03:48:02 +1000

    getting to understand how to structure the project and using react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,22 @@
 
 import './App.css';
+import {Routes,Route} from 'react-router-dom';
+
+import Nav from './components/common/nav/Nav';
+
+import Feed from './feed/Feed';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+   <>
+    <Nav/>
+    <div className='container'>
+        <Routes>
+          <Route path='/' element={Feed}/>
+        </Routes>
+    </div>
+   
+   </>
   );
 }
 
